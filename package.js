04efd3f6fd9de8c665644d4e35031ecb8d174400
Colin Fletcher <colinafletcher@gmail.com>
2021-01-08 14:21:44 -0800

    Updated dependencies

--- package.js
+++ package.js
@@ -1,21 +1,17 @@
 Package.describe({
   name: 'felipems:autoform-semantic-ui',
-  version: '0.9.6',
-  // Brief, one-line summary of the package.
+  version: '0.10.0',
   summary: 'Semantic-ui template for aldeed:autoform package.',
-  // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/felipesmendes/meteor-autoform-semantic-ui.git',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  git: 'https://github.com/cheesington/meteor-autoform-semantic-ui.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.0.1');
+  api.versionsFrom('1.7');
 
-  api.use(['ecmascript', 'templating', 'underscore', 'jquery'], 'client');
+  api.use(['ecmascript', 'templating', 'underscore', 'jquery@3.0.0'], 'client');
 
-  api.use('aldeed:autoform@5.8.1', 'client');
+  api.use('aldeed:autoform@7.0.0', 'client');
 
   api.addFiles([
     // semantic-ui Template - General
